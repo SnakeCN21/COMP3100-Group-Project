f46c4579feb14487f2830fc26d8509385cd144a1
Marklin97 <48002791+marklin97@users.noreply.github.com>
2020-04-14 21:00:13 +1000

    client.java --new version

--- Stage 1/Client.java
+++ Stage 1/Client.java
@@ -5,131 +5,132 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.*;
 import java.util.*;
 
-
-
 public class Client {
 
- private Socket socket = null;
- private BufferedReader input = null;
- private DataOutputStream output = null;
- private String largestServer= null;
+  private Socket socket = null;
+  private BufferedReader input = null;
+  private DataOutputStream output = null;
+  private String largestServer = null;
 
+  // system commands
+  public static final String HELO = "HELO";
+  public static final String AUTH = "AUTH ";
+  public static final String REDY = "REDY";
+  public static final String SCHD = "SCHD";
+  public static final String OK = "OK";
+  public static final String QUIT = "QUIT";
+  public static final String JOBN = "JOBN";
 
- public Client(String address, int port) throws Exception {
-  socket = new Socket(address, port);
-  System.out.println("Connected");
-  // reads input from the socket input stream
-  input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+  public Client(String address, int port) throws Exception {
+    socket = new Socket(address, port);
+    System.out.println("Connected");
+    // reads input from the socket input stream
+    input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-  // sends output to the socket output stream
-  output = new DataOutputStream(socket.getOutputStream());
+    // sends output to the socket output stream
+    output = new DataOutputStream(socket.getOutputStream());
 
- }
+  }
 
- void message(String msg) throws IOException {
-  byte[] message = msg.getBytes();
-  output.write(message, 0, message.length);
-  output.flush();
+  void message(String msg) throws IOException {
+    byte[] message = msg.getBytes();
+    output.write(message, 0, message.length);
+    output.flush();
+  }
 
- }
-
- //receive message from server
+  // receive message from server
   String getFromServer() throws Exception {
-  StringBuilder msg = new StringBuilder();
-  while(msg.length()<1){
-while(input.ready()){
-   msg.append((char) input.read());
-  }
-}
-  String inputMsg = msg.toString();
-  return inputMsg;
- }
-
- void getLargest() {
-  String largestType = null;
-  try {
-   File inputFile = new File("system.xml");
-   DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-   DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-   Document doc = dBuilder.parse(inputFile);
-   doc.getDocumentElement().normalize();
-   NodeList serverList = doc.getElementsByTagName("server");
-   Node tempNode = serverList.item(0);
-   Element tempElement = (Element) tempNode;
-   int largestCore = Integer.parseInt(tempElement.getAttribute("coreCount"));
-
-   // looping through the list
-
-   for (int temp = 0; temp < serverList.getLength(); temp++) {
-    Node currentNode = serverList.item(temp);
-    if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
-     Element currentElement = (Element) currentNode;
-     int coreCount = Integer.parseInt(currentElement.getAttribute("coreCount"));
-     if (coreCount > largestCore) {
-      largestCore = coreCount;
-      largestType = currentElement.getAttribute("type");
-     }
+    StringBuilder msg = new StringBuilder();
+    while (msg.length() < 1) {
+      while (input.ready()) {
+        msg.append((char) input.read());
+      }
     }
-   }
-
-  } catch (Exception e) {
-   e.printStackTrace();
+    String inputMsg = msg.toString();
+    return inputMsg;
   }
-  largestServer = largestType;
- }
 
- public static void main(String[] args) {
-  try {
+  void getLargest() {
+    String largestType = null;
+    try {
+      File inputFile = new File("system.xml");
+      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+      Document doc = dBuilder.parse(inputFile);
+      doc.getDocumentElement().normalize();
+      NodeList serverList = doc.getElementsByTagName("server");
+      Node tempNode = serverList.item(0);
+      Element tempElement = (Element) tempNode;
+      int largestCore = Integer.parseInt(tempElement.getAttribute("coreCount"));
 
-   String name = System.getProperty("user.name");
+      // looping through the list
 
-   Client client = new Client("127.0.0.1", 50000);
-
-   client.message("HELO");
-   client.getFromServer();
-
-   client.message("AUTH " + name);
-   client.getFromServer();
-
-   client.message("REDY");
-   String msg = client.getFromServer();
-   client.getLargest();
-
-   // Requesting for all server information
-   while (!msg.equals("NONE")) {
-    String[] line = msg.split(" ");
-    String request = ("RESC All " + line[4] + " " + line[5] + " " + line[6]);
-    client.message(request);
-
-    if (client.getFromServer().equals("DATA")) {
-     String temp = "";
-     client.message("OK");
-
-
-     while (!temp.equals(".")) {
-
-      if (!temp.equals(".")) {
-       client.message("OK");
-       temp = client.getFromServer();
+      for (int temp = 0; temp < serverList.getLength(); temp++) {
+        Node currentNode = serverList.item(temp);
+        if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+          Element currentElement = (Element) currentNode;
+          int coreCount = Integer.parseInt(currentElement.getAttribute("coreCount"));
+          if (coreCount > largestCore) {
+            largestCore = coreCount;
+            largestType = currentElement.getAttribute("type");
+          }
+        }
       }
 
-
-     }
-	if(client.largestServer!=null){
-     client.message("SCHD" + " " + line[2] + " " + client.largestServer + " " + "0");
-}
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    client.getFromServer();
-    client.message("REDY");
-    msg = client.getFromServer();
-   }
-   client.message("QUIT");
-   client.input.close();
-   client.output.close();
-   client.socket.close();
-
-  } catch (Exception e) {
-   //System.out.println(e);
+    largestServer = largestType;
   }
- }
+
+  public static void main(String[] args) {
+    try {
+
+      String name = System.getProperty("user.name");
+
+      Client client = new Client("127.0.0.1", 50000);
+
+      client.message(HELO);
+      client.getFromServer();
+
+      client.message(AUTH + name);
+      client.getFromServer();
+
+      client.message(REDY);
+      String msg = client.getFromServer();
+      client.getLargest();
+
+      // Requesting for all server information
+      while (!msg.equals("NONE")) {
+        String[] line = msg.split(" ");
+        String request = ("RESC All " + line[4] + " " + line[5] + " " + line[6]);
+        client.message(request);
+
+        if (client.getFromServer().equals("DATA")) {
+          String temp = "";
+          client.message(OK);
+
+          while (!temp.equals(".")) {
+            if (!temp.equals(".")) {
+              client.message(OK);
+              temp = client.getFromServer();
+            }
+          }
+          if (client.largestServer != null) {
+            client.message(SCHD + " " + line[2] + " " + client.largestServer + " " + "0");
+          }
+        }
+        client.getFromServer();
+        client.message(REDY);
+        msg = client.getFromServer();
+      }
+      client.message(QUIT);
+      client.input.close();
+      client.output.close();
+      client.socket.close();
+
+    } catch (Exception e) {
+      // System.out.println(e);
+    }
+  }
 }
